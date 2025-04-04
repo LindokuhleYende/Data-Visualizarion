0802de08e8f6d85c04c090bdc615cb70361ca4b9
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-04 21:51:31 +0200

    Update heatmap_visual.py

---
+++ flightDelay/heatmap_visual.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+filepath = "flightDelay/flight_delays.csv"
+
+flight_data = pd.read_csv(filepath, index_col="Month")
+print(flight_dat)
