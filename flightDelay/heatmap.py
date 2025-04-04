1ce9f5fc3c0bff8e3501b6a23a91b27775cabe3b
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-04 22:20:40 +0200

    Update heatmap.py

--- flightDelay/heatmap.py
+++ flightDelay/heatmap.py
@@ -7,3 +7,10 @@ filepath = "flightDelay/flight_delays.csv"
 
 flight_data = pd.read_csv(filepath, index_col="Month")
 print(flight_data)
+
+plt.figure(figsize=(14,7))
+plt.title("Average Arrival Delay for Each Airline, by Month")
+# Heatmap showing average arrival delay for each airline by month
+sns.heatmap(data=flight_data, annot=True)
+plt.xlabel("Airline")
+plt.show()
