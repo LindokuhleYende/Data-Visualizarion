d5112268532d3acb6090adddb1c3601017cb6422
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-04 21:25:19 +0200

    Update visual.py

--- flightDelay/visual.py
+++ flightDelay/visual.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-filepath = "/flight_delays.csv"
+filepath = "flightDelay/flight_delays.csv"
 
 flight_data = pd.read_csv(filepath, index_col="Month")
 #the name of the column that will be used to index the rows is "Month"
 
 print(flight_data)
+
+#bar chart showing the average arrival delay for 
+# Spirit Airlines (airline code: NK) flights, by month
+
