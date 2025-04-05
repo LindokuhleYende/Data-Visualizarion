8ca0cdb2abdbd48db8d34d44112f3f63482f13d6
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-05 20:04:08 +0200

    Update line.py

--- flightDelay/line.py
+++ flightDelay/line.py
@@ -4,6 +4,6 @@ import seaborn as sns
 
 filepath = "flightDelay/flight_delays.csv"
 df = pd.read_csv(filepath, index_col="Month")
-
+plt.title("TheAverageDelayOfFlights,Permothh")
 sns.lineplot(data=df)
 plt.show()
