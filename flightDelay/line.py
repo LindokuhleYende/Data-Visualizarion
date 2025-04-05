4f38077a19d6ecc5ddadc2965a625897981e0807
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-05 19:59:33 +0200

    Update line.py

---
+++ flightDelay/line.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+filepath = "flightDelay/flight_delays.csv"
+df = pd.read_csv(filepath, index_col="Month")
+
+sns.lineplot(data=df)
+plt.show()
