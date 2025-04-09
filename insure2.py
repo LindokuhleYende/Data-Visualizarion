ae24e6990fe38ac369e3c06fc4bffc320861864d
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-09 15:22:34 +0200

    Update insure2.py

--- insure2.py
+++ insure2.py
@@ -11,5 +11,7 @@ insure_data = pd.read_csv(filepath)
 #does it also influence the charges of insurance
 #sns.scatterplot(x=insure_data['bmi'], y=insure_data['charges'], hue=insure_data['smoker'])
 sns.lmplot(x="bmi", y="charges", hue="smoker", data=insure_data)
+sns.swarmplot(x=insure_data['smoker'],
+              y=insure_data['charges'])
 plt.title("BMI of smokers and non-smokers VS The Insurance Charges")
 plt.show()
